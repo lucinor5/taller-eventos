e90e5296226554a93a5c7f2ef2e0e6e5875759ff
lucinor5 <141081049+lucinor5@users.noreply.github.com>
2023-08-19 20:42:42 -0300

    alerta div

---
+++ eventos.js
@@ -0,0 +1,9 @@
+function createAlert() {
+    window.alert("Hola! Soy el div");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    let btn = document.getElementById('btnSaludar');
+    btn.addEventListener("click", createAlert); 
+});
+
